42026f0aae7b90953f0b299075570c87ceb8a28b
manuelMarkDenver <manuel.markdenver@gmail.com>
2022-12-16 16:55:31 +0800

    updates

--- frontend/components/home/banner/index.jsx
+++ frontend/components/home/banner/index.jsx
@@ -8,11 +8,11 @@ import imageLogo from "../../../public/assets/images/image-logo.png";
 
 const Banner = () => {
   return (
-    <Container sx={{ position: "relative",  }}>
+    <Box sx={{ position: "relative",  }}>
       <Image src={bannerImg} style={{ objectFit: "cover", width: "100%", height: "100vh", zIndex: 5}} />
       <Image src={imageLogo} style={{ position: "absolute", right: "0", bottom: "0", height: "60%", width: "auto", zIndex: 10}} />
       <Box sx={{ position: "absolute", top: "0", left: "0", right: "0", bottom: "0", background: "pink", opacity: "45%", zIndex: 0 }}></Box>
-    </Container>
+    </Box>
   );
 };
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,4 @@
 import Layout from "../components/layout/layout";
-import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
